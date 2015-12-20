0421616c3f4566848a50fade30bd4bb9c805b7a0
Omaar22 <o-mohamed@hotmail.com>
2015-12-20 05:14:06 +0200

    Create Message.h

---
+++ Message.h
@@ -0,0 +1,34 @@
+//
+// Created by Omar Mohamed on 19/12/2015.
+//
+
+#ifndef SOCIAL_MESSAGE_H
+#define SOCIAL_MESSAGE_H
+
+#include <string>
+
+using namespace std;
+
+
+
+class Message {
+private:
+    string text, receiver, sender;
+    // DateTime
+public:
+    void view();
+
+    Message(string text, string receiver, string sender);
+
+    string getSender();
+
+    string getReceiver();
+
+    string getText();
+
+};
+
+
+#endif //SOCIAL_MESSAGE_H
+
+
