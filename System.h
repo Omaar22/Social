45082aba3898e6fdb4f896468c1bf131da37544a
Omaar22 <o-mohamed@hotmail.com>
2015-12-25 02:28:07 +0200

    Update System.h

--- System.h
+++ System.h
@@ -5,19 +5,27 @@
 
 class System {
 private:
-    vector<User> userList;
+    User *userList;
+    int userListSize;
+    int userListTotalSize;
 
     bool validPassword(string email, string password); // to sign in
     bool validInfo(User); // to sign up
 
+    void merge(User arr[], int L, int R);
+    void mergeSort(User arr[], int L, int R);
+
+    int find(int, int, string); // binary search return index or -1 if not found
+
 public:
+    System();
     User *loggedInUser; // point to a user in userList
+    void sortUsers(); // merge sort
 
     User search(string); // return new user if not found
-    int find(string); // return index or -1 if not found
 
 
-    bool isFriend(User other); // is friend with the logged in user
+    bool isFriend(User other); // is friend with the logged in user // shoud be in user?
 
     void viewUSer(User aUser); // nas2sa
 
@@ -26,7 +34,7 @@ public:
     void signIn(string email, string password);
 
 
-    void removeAccount(User); // na2sa
+    void removeAccount(User);
 
     void readFromFile();
     void writeOnFile();
