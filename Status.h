6a309ab8e5fd5deb86c3ccd9eb8763c848a847ec
Maryam El-Sokkary <maryam.sokkary@gmail.com>
2015-12-27 23:57:48 +0200

    Create Status.h

---
+++ Status.h
@@ -0,0 +1,29 @@
+#ifndef STATUS_H_
+#define STATUS_H_
+#include <bits/stdc++.h>
+#include "Post.h"
+#include "DateTime.h"
+
+using namespace std;
+
+class Status :public Post {
+private:
+	string feelings [10] = {"sad", "mad", "happy", "determined"};
+	int size = 4;
+	int choice;
+	string text;
+	string feeling;
+	string time;
+
+public:
+	Status();
+	Status (string, int);
+	void viewFeelings ();
+	void addFeeling (int);
+	void addStatus (string);
+	//void edit ();
+	virtual void view ();
+	virtual ~Status();
+};
+
+#endif /* STATUS_H_ */
