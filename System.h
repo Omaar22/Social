aca3fe8260fb73eb51c15c6665c1e83e27d2bf7c
seifgamal <seif_gamal09@yahoo.com>
2015-12-27 19:08:08 +0200

    Update System.h

--- System.h
+++ System.h
@@ -8,22 +8,23 @@ class System
 {
 public:
     System();
-    User search(string); // return new user if not found
+    User searchUser(string); // return new user if not found
     bool strongPassword(string P);
     bool validEmail(string P);
     bool validName(string P);
     bool validAccount() ;
+    int getNum();
 
     void merge(User *a, int n, int m) ;
     void merge_sort(User *a, int n) ;
     bool validInfo(User U) ;
     void  AddUser(User U) ;
-    void Sort() ;
     void show();
     void signUp();
     void removeAccount(User user) ;
     int findUser(int, int, string); // binary search return index or -1 if not found
     void signIn() ;
+    User loggedInUser ;
 
     ~System();
 protected:
