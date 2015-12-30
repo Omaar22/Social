6fdc70fa58d4c1e199cf3d1290beb499ed07cc0a
Omaar22 <o-mohamed@hotmail.com>
2015-12-30 11:45:08 +0200

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -1,71 +1,54 @@
 #include "System.h"
-#include "User.h"
 
-System::System() : numOfUsers(0) , listOfUsers( new User[100] ){}
-System::~System() {}
+System::System() : numOfUsers(0), listOfUsers(NULL), loggedInUser(NULL) {
 
-bool System::validName(string s)
-{
-    for (int i = 0 ; i < s.size()  ; i++)
-    {
-        if ( (s[i] >= 65 && s[i] <= 90 ) || (s[i] >= 97 && s[i] <= 122) || s[i] == ' ')
-            continue ;
-        else
-            return 0 ;
-    }
-    return s.size() ;
+    // get data from file
+}
+
+bool System::validName(string name) {
+
+    for (char X : name)
+        if (!('a' <= X && X <= 'z' || 'A' <= X && X <= 'Z' || X == ' '))
+            return 0;
+
+    return name.size() != 0;
 }
 
 ////////////////////////
 
-bool System::strongPassword(string P)
-{
-    int sz = int (P.size()) ;
-    bool U , L , D ;
-    for (int i = 0 ; i < sz ; i++)
-    {
+bool System::strongPassword(string P) {
+    int sz = (int) P.size();
+    bool U = 0, L = 0, D = 0;
+    for (int i = 0; i < sz; i++) {
         if (islower(P[i]))
-            L = true ;
+            L = true;
         else if (isupper(P[i]))
-            U = true ;
+            U = true;
         else if (isdigit(P[i]))
-            D = true ;
+            D = true;
     }
-    return sz >= 8 && L && U && D ;
+    return sz >= 8 && L && U && D;
 }
 
 ////////////////////////
 
-bool System::validEmail(string P)
-{
-    int f1 = P.rfind('@') ;
-    int f2 = P.rfind(".com") ;
-    int f3 = P.find(' ') ;
-    return  f1 != -1 && f2 != -1 && (f2 - f1) > 1  && f3 == -1;
-}
-
-/////////////////////
-
-bool  System:: validAccount()
-{
-    return numOfUsers < 100 ;
+bool System::validEmail(string P) {
+    int f1 = P.rfind('@');
+    int f2 = P.rfind(".com");
+    int f3 = P.find(' ');
+    return f1 != -1 && f2 != -1 && (f2 - f1) > 1 && f3 == -1;
 }
 
 ///////////////////
 
-void System:: AddUser(User U)
-{
-    if( numOfUsers > 100 ) cout << "Sorry, You can't sign up with us :( ";
-    else {
-        listOfUsers[ numOfUsers++ ] = U ;
-        merge_sort(listOfUsers, numOfUsers );
-    }
+void System::AddUser(User U) {
+    listOfUsers[numOfUsers++] = U;
+    merge_sort(listOfUsers, numOfUsers);
 }
 
 ///////////////////
 
-void System::merge_sort(User *a, int n)
-{
+void System::merge_sort(User *a, int n) {
     if (n < 2)
         return;
     int m = n / 2;
@@ -74,29 +57,19 @@ void System::merge_sort(User *a, int n)
     merge(a, n, m);
 }
 
-////////////////////
-
-bool System::validInfo(User U) // not completed !
-{
-    bool e = validEmail(U.getEmail()) ;
-}
-
 /////////////////
 
-void System::merge(User *a, int n, int m)
-{
+void System::merge(User *a, int n, int m) {
     int i = 0, j = m;
     User *b = new User[n];
-    for (int k = 0; k < n; ++k)
-    {
+    for (int k = 0; k < n; ++k) {
         if (i == m)
             b[k] = a[j++];
         else if (j == n)
             b[k] = a[i++];
         else if (a[i].getName() <= a[j].getName())
             b[k] = a[i++];
-        else
-        {
+        else {
             b[k] = a[j++];
         }
     }
@@ -107,31 +80,33 @@ void System::merge(User *a, int n, int m)
 
 /////////////////
 
-int System::findUser(int lower, int upper, string email)
-{
+int System::findUser(int lower, int upper, string email) {
     if (lower > upper)
         return -1;
     int mid = (lower + upper) / 2;
+
     if (listOfUsers[mid].getEmail() > email)
         return findUser(lower, mid - 1, email);
-    else if (listOfUsers[mid].getEmail() < email)
+    if (listOfUsers[mid].getEmail() < email)
         return findUser(mid + 1, upper, email);
-    else return mid;
+    return mid;
 }
 
 /////////////////////
 
-void System::show() ///testing
+void System::viewAllUsers() ///testing
 {
-    for (int i = 0 ; i <numOfUsers ; i++)
-        cout << listOfUsers[i].getName() <<endl;
+    for (int i = 0; i < numOfUsers; i++)
+        cout << listOfUsers[i].getName() << endl;
 }
 
 /////////////////
 
-void System::removeAccount(User user )
-{
+void System::removeAccount(User user) {
     int index = findUser(0, numOfUsers - 1, user.getEmail());
+
+    if (index == -1)
+        throw "User Not Found";
 
     numOfUsers--;
 
@@ -142,109 +117,112 @@ void System::removeAccount(User user )
 
 ///////////////
 
-void System::signUp()
-{
-    if (!validAccount())
-    {
-        cout << "Sorry ,You cannot sign up\n";
-        return ;
-    }
+void System::signUp() {
     cout << "\t\t\t Welcome ..\nSign Up :\n\n";
-    User u ;
-    string s;
-    while(1)
-    {
+    User newUser;
+    while (1) {
         cout << "Fill these Information\n" << endl;
-        try
-        {
-            int choice ;
+        try {
+            string input = "";
+
             cout << "Name : ";
+            while (input == "")
+                getline(cin, input);
+            if (!validName(input))
+                throw "Invalid name.";
+            newUser.setName(input);
 
-            s = "";
-            while(s == "")
-                getline(cin , s);
-            u.setName(s) ;
             cout << "Password : ";
-            cin >> s;
-            u.setPassword(s) ;
-            cout << "Gender (1 for Male , 2 for Female ) : ";
-            cin >> choice;
+            cin >> input;
+            if (!strongPassword(input))
+                throw "Weak Password.";
+            newUser.setPassword(input);
 
-            u.setGender(choice) ;
-            cout << "Email : " ;
-            cin >> s ;
-            u.setEmail(s);
-            string month , day , year ;
+            cout << "Gender (1 for Male , 2 for Female ) : ";
+            int choice;
+            cin >> choice;
+            if (choice != 1 && choice != 2)
+                throw "Wrong Choice.";
+            newUser.setGender(choice);
+
+            cout << "Email : ";
+            cin >> input;
+            if (!validEmail(input))
+                throw "Invalid E-mail.";
+            newUser.setEmail(input);
+
+            string month, day, year;
             cout << "Enter day month year\n";
-            cin >> day >> month >> year ;
+            cin >> day >> month >> year;
             if (day.size() == 1)
-                day = '0' + day ;
+                day = '0' + day;
             if (month.size() == 1)
-                month = '0' + month ;
-            string s = day + '/' + month + '/' + year ;cout << s <<endl;
-            u.setBirthDate(s);
+                month = '0' + month;
+            string date = day + '/' + month + '/' + year;
+            if (!validDate(input))
+                throw "Invalid Date.";
+            newUser.setBirthDate(date);
 
             cout << endl;
             break;
-
         }
-        catch(const char *s)
-        {
-            cout << s << endl;
-            continue ;
+        catch (const char *error) {
+            cout << error << endl;
         }
     }
 
-    AddUser(u);
-    merge_sort(listOfUsers , numOfUsers) ;
+    AddUser(newUser);
 }
-void System::signIn()
-{
-    string e , p ;
 
-    cout << "Email: " ;
-    cin >> e ;
-    int idx  = findUser(0 , numOfUsers , e) ;
-    while (idx == -1)
-    {
-        cout << "Email does not exist please Enter it again :\nEmail:" ;
-        cin >> e ;
-        idx  = findUser(0 , numOfUsers , e) ;
+void System::signIn() {
+    string email, password;
+    cout << "Email: ";
+    cin >> email;
+    cout << "Password: ";
+    cin >> password;
+
+    int index = findUser(0, numOfUsers - 1, email);
+
+    while (index == -1 || listOfUsers[index].getPassword() != password) {
+        cout << "Invalid email or password .. please try again\n";
+        cout << "Email: ";
+        cin >> email;
+        cout << "Password: ";
+        cin >> password;
+        index = findUser(0, numOfUsers - 1, email);
     }
-    cout << "Password :";
-    cin >> p;
-    while(listOfUsers[idx].getPassword() != p )
-    {
-        cout << "Invalid password .. please Enter again : ";
-        cin >> p ;
-    }
-    loggedInUser = listOfUsers[idx] ;
+
+    loggedInUser = &listOfUsers[index];
 }
 
 /////////////////////
 
 
-User System::searchUser(string e)
-{
-    for (int i = 0 ; i < numOfUsers ; i++ )
-    {
-        if (listOfUsers[i].getEmail() == e)
-            return listOfUsers[i];
-    }
-    User U;
-    U.setName("NoUser") ;
-    return U ;
+User System::searchUser(string email) {
+    int index = findUser(0, numOfUsers - 1, email);
+    if (index == -1)
+        throw "User Not Found";
+    return listOfUsers[index];
 }
 
 ///////////////////////
 
-bool System::validDate(string s)
-{
-    int sz = s.size() ;
-    int day = (int(s[0] - '0') * 10 + int(s[1] - '0')) ;
-    int month = (int(s[3] - '0') * 10 + int(s[4] - '0')) ;
-    int year = (int(s[6] - '0') * 1000 + int(s[7] - '0') * 100 + int(s[8] - '0') * 10 + int(s[9] - '0')) ;
-    return  isdigit(s[0]) && isdigit(s[1]) && isdigit(s[3]) && isdigit(s[4])
+bool System::validDate(string s) {
+    if (s.size() != 10)
+        return false;
+
+    int day = (int(s[0] - '0') * 10 + int(s[1] - '0'));
+    int month = (int(s[3] - '0') * 10 + int(s[4] - '0'));
+    int year = (int(s[6] - '0') * 1000 + int(s[7] - '0') * 100 + int(s[8] - '0') * 10 + int(s[9] - '0'));
+    return isdigit(s[0]) && isdigit(s[1]) && isdigit(s[3]) && isdigit(s[4])
            && isdigit(s[6]) && isdigit(s[7]) && isdigit(s[8]) && isdigit(s[9]) && s[2] == '/' && s[5] == '/'
-           &&  day <= 31 && day >= 1 &&  month <= 12 && month >= 1 && year >= 1900 && year <= 2014  ;
+           && day <= 31 && day >= 1 && month <= 12 && month >= 1 && year >= 1900 && year <= 2014;
+}
+
+///////////////
+
+System::~System() {
+    // call write on file function ..
+    delete[]listOfUsers;
+    delete loggedInUser;
 }
