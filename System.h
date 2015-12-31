3925c23d1558050aba0545ea4d92c13736397e55
SalmaSamy <salma.al.emam96@gmail.com>
2015-12-31 14:44:25 +0200

    Update System.h

--- System.h
+++ System.h
@@ -1,35 +1,39 @@
-#ifndef System_H
-#define System_H
-
+#ifndef SYSTEM_H
+#define SYSTEM_H
+#include <iostream>
 #include "User.h"
+using namespace std ;
 
-class System {
-private:
-    User *listOfUsers;
-    int numOfUsers = 0;
-
-    int findUser(int lower, int upper, string email); // binary search return index or -1 if not found
-
-    bool strongPassword(string P);
-    bool validEmail(string P);
-    bool validName(string P);
-    bool validDate(string s);
-    void AddUser(User U);
-
-    void merge(User *a, int n, int m);
-    void merge_sort(User *a, int n);
+class System
+{
 public:
-    User *loggedInUser;
+    System();
 
-    System(); // get data from file
+    void startMinue();
 
+    static bool strongPassword(string P);
+    static bool validEmail(string P);
+    static bool validName(string P);
+    static bool validDate(string );
+    bool validAccount();
+
+    void merge(User *a, int n, int m) ;
+    void merge_sort(User *a, int n) ;
+
+    int findUser(string);
+    void AddUser(User U) ;
+    User searchUser(string);
+    void removeAccount(User user) ; /// not complete
+
+    ~System();
+
+private:
+    User *listOfUsers ;
+    int numOfUsers = 0 ;
+    User loggedInUser ;
     void signUp();
-    void signIn();
-    User searchUser(string); // throw an exception if not found
-    void removeAccount(User user); // na2sa
-    void viewAllUsers(); // print names only
+    void signIn() ;
 
-    ~System();  // call write on file function
 };
 
-#endif // System_H
+#endif // SYSTEM_H
