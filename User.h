a90df24badc5dd5e29dc22fc4fa894f659bceb22
Omaar22 <o-mohamed@hotmail.com>
2016-01-02 17:13:47 +0200

    Update User.h

--- User.h
+++ User.h
@@ -1,69 +1,72 @@
 #ifndef USER_H
 #define USER_H
+
 #include "Post.h"
 #include "Status.h"
 #include "Message.h"
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
-class User
-{
+class User {
 
-    private:
+private:
 
-        string name, password, gender, email, birthDate ;
-        int numOfFriends ;
-        int numOfPosts ;
-        string interests[3] ;
-        User **friends;
-        Post **posts;
+    string name, password, gender, email, birthDate;
+    int numOfFriends;
+    int numOfPosts;
+    string interests[3];
+    User **friends;
+    Post **posts;
 
-        vector <Message> receivedMessages;
-        vector <Message> sentMessages;
-
-        void putInPosts( Post *P ) ;
-
-    public:
-
-        void viewInfo(string);
-        void editInfo();
-        bool isFriend( string );
-        void viewFriends();
-        void addFriend( User* ) ;
-        void removeFriend( string ) ;
-        User getFriends();
-
-        User searchUser( string );
-
-        void addPost();
-        void viewPosts();
-        void viewUser( User* );
-
-        void viewReceivedMessages();
-        void viewSentMessages();
-
-        void sendMessage(Message);
-        void recieveMessage(Message);
-        void sendMessageFun(User*);
-
-        void setName( string );
-        string getName();
-        void setPassword( string );
-        string getPassword();
-        void setEmail( string );
-        string getEmail();
-        void setGender( int );
-        string getGender();
-        void setBirthDate( string );
-        string getBirthDate();
-        int getNumOfFriends();
+    vector<Message> receivedMessages;
+    vector<Message> sentMessages;
 
 
+public:
+    void putInPosts(Post *P);
 
-        User();
-        ~User();
+    void viewInfo(string);
+    void editInfo();
+    bool isFriend(string);
+    void viewFriends(User *);
+    void addFriend(User *);
+    void removeFriend(string);
 
+    void addPost();
+    void viewPosts(User *);
+    void viewUser(User *, User *);
+    User searchUser(string);
+
+    void viewReceivedMessages();
+    void viewSentMessages();
+    vector<Message> getReceivedMessages();
+    vector<Message> getsentMessages();
+
+    void sendMessage(Message);
+    void recieveMessage(Message);
+    void sendMessageFun(User *);
+
+    void setName(string);
+    string getName();
+    void setPassword(string);
+    string getPassword();
+    void setEmail(string);
+    string getEmail();
+    void setGender(int);
+    string getGender();
+    void setBirthDate(string);
+    string getBirthDate();
+    int getNumOfFriends();
+
+
+    User();
+    ~User();
+
+    User *getFriends(int index);
+    int getNumOfPosts();
+    Post *getPost(int index);
 };
 
 #endif
