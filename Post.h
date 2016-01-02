5428c51715ebcd98ff2f7dcfffd64ae24beecadd
Omaar22 <o-mohamed@hotmail.com>
2016-01-02 17:10:03 +0200

    Update Post.h

--- Post.h
+++ Post.h
@@ -1,38 +1,48 @@
-//updated 31 DEC 2015
-//Maryam El-Sokkary
-
-#include <iostream>
-#include <cstring>
-#include "DateTime.h"
+#include <bits/stdc++.h>
 #include "User.h"
+#include  "DateTime.h"
 using namespace std;
 
+#ifndef POST_H
+#define POST_H
 
-#ifndef POST_H_
-#define POST_H_
 
 class Post {
 
 protected:
-int likeCounter;
-int dislikeCounter;
-int numberOfComments;
-string *comments;
-string *likers;
-string *dislikers;
-DateTime dateandtime;
+    int likeCounter;
+    int dislikeCounter;
+    int numberOfComments;
+    string *comments;
+    string *likers;
+    string *dislikers;
+    DateTime dateandtime;
 
 
 public:
-	Post();
-	void like (string);
-	void dislike (string);
-	void viewComments ();
-	void viewPostdetails();
-	void addcomment (string);
-	void deleteComment(int);
-	virtual void view () = 0;
-	virtual ~Post();
+    Post();
+    void like(string);
+    void dislike(string);
+    void viewComments();
+    void viewPostdetails();
+    void addcomment(string);
+    void addcomment(string, string Date);
+    void deleteComment(int);
+    void viewLikers();
+    virtual void upVote(string, int);
+    virtual void downVote(string, int);
+    virtual void view() = 0;
+    virtual ~Post();
+
+    int getLikeCounter();
+    int getDislikeCounter();
+    int getNumberOfComments();
+    string getComment(int index);
+
+    void setLikeCount(int likeCount);
+    void setDislikeCount(int dislikeCount);
 };
 
+
 #endif /* POST_H_ */
+
